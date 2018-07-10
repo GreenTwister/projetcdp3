4bbe4ad691405f14f22a1127318d9060db33340c
Patrick Sabaté <37292675+GreenTwister@users.noreply.github.com>
2018-07-10 10:39:09 +0200

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -7,7 +7,7 @@ if(!class_exists('Manager')){
 
 	    public function dbConnect()
 	    {
-	        $db = new \PDO('mysql:host=db745083302.db.1and1.com;dbname=db745083302;charset=utf8', 'dbo745083302', 'Patrockthebest2084!');
+	        $db = new \PDO('mysql:host=localhost;dbname=superbase;charset=utf8', 'pseudo', 'mot de passe');
 	        return $db;
 	    }
 
